2fc5712c08ff1c1c24378eea3fbc8efde3f8ba9e
ekdldksp123 <ekdldksp123@naver.com>
2021-08-21 23:33:40 +0900

    NOON-28 page6 multi cropper

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'page8/word_list.dart';
 import 'settings.dart';
 import 'page7/multi_images_process.dart';
 import 'cropper.dart';
+import 'page6/multi_cropper.dart';
 import 'mynote.dart';
 
 void main() {
@@ -19,13 +20,14 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/mynote',
+      initialRoute: '/multi_cropper',
       routes: {
+
         '/main': (context) => DemoPage(),
         '/pick': (context) => Gallery(),
         '/settings': (context) => Settings(),
         '/multi' : (context) => MultiImagesProcess(),
-        '/cropper': (context) => MyCropper(),
+        '/multi_cropper': (context) => MultiCropper(),
         '/mynote' : (context) => MyApp()
       },
     );
